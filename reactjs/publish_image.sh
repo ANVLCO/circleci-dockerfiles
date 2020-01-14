56fdd0f79983c496e7516c7cbb16d91be29fa5e2
John Ellis <john@anvl.com>
2020-01-14 14:26:44 -0500

    Typo fix

--- reactjs/publish_image.sh
+++ reactjs/publish_image.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 
 OWNER='anvlco'
-REPOSITORY='insights'
+REPOSITORY='circleci-dockerfiles'
 
 docker build -t "docker.pkg.github.com/$OWNER/$REPOSITORY/reactjs:12.11" .
 docker push "docker.pkg.github.com/$OWNER/$REPOSITORY/reactjs:12.11"
