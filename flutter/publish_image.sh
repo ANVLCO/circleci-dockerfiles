363d996b5f41f666349b2180ac8d2c29008c6028
Bill Barbour <brbatwork@gmail.com>
2021-05-04 13:55:05 -0400

    upgrade to flutter 2.0.6

--- flutter/publish_image.sh
+++ flutter/publish_image.sh
@@ -3,5 +3,5 @@
 OWNER='anvlco'
 REPOSITORY='circleci-dockerfiles'
 
-docker build -t "docker.pkg.github.com/$OWNER/$REPOSITORY/flutter:1.22.4" .
-docker push "docker.pkg.github.com/$OWNER/$REPOSITORY/flutter:1.22.4"
+docker build -t "docker.pkg.github.com/$OWNER/$REPOSITORY/flutter:2.0.6" .
+docker push "docker.pkg.github.com/$OWNER/$REPOSITORY/flutter:2.0.6"
