1f2916483a6fba0f6351d6e5c8137ea1f3caeaee
Sam Pontecorvo <samuelpontecorvo@gmail.com>
2021-06-10 15:26:49 -0400

    Upgrade to node 14. (#10)
    * Upgrade to node 14.
    
    * Upgrade serverless.
    
    * fix typo.
    
    Co-authored-by: smpontec <smpontec>

--- node/publish_image.sh
+++ node/publish_image.sh
@@ -3,5 +3,5 @@
 OWNER='anvlco'
 REPOSITORY='circleci-dockerfiles'
 
-docker build -t "docker.pkg.github.com/$OWNER/$REPOSITORY/node:12.11" .
-docker push "docker.pkg.github.com/$OWNER/$REPOSITORY/node:12.11"
+docker build -t "docker.pkg.github.com/$OWNER/$REPOSITORY/node:14" .
+docker push "docker.pkg.github.com/$OWNER/$REPOSITORY/node:14"
